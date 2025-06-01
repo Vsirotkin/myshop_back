d4eb93026da186bc1fd5f223595140bf237dc5e0
Vik Sir <vsirotkin15@gmail.com>
2025-06-01 12:01:22 +0300

    superuser, category and products

---
+++ myshop_back/src/shop/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+# from shop.views import views
+
+app_name = 'shop'
+
+# urlpatterns = [
+#     path('', views.ProductListView.as_view(), name='product_list'),
+    
+#     path('category/<slug:category_slug>/', 
+#          views.ProductListView.as_view(), 
+#          name='product_list_by_category'),
+    
+#     path('product/<int:pk>/<slug:slug>/', 
+#          views.ProductDetailView.as_view(), 
+#          name='product_detail'),
+# ]
